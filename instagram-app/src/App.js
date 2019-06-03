a74a406d400a500086fbf5c95c61d91b2c1f6815
unknown <robertrey1234@gmail.com>
2019-06-03 18:08:28 -0500

    created base for search bar

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
+import dummyData from '../dummy-data.js'
 import './App.css';
+import searchbar from './components/SearchBar/searchbar';
 
 function App() {
   return (
---
+++ instagram-app/src/components/SearchBar/searchbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class searchbar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default searchbar
